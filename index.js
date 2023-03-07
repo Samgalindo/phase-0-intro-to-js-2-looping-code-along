8b1a41a19c2cec5410aa7edffa16beaf9e38a4a5
Sam Galindo <ovosamgalindo@gmail.com>
2023-03-06 19:23:35 -0800

    Completed tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let messages = []
+
+    for (let i = 0; i < names.length; i++){
+        
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+   
+    return messages;
+}
+
+let names = ["Jafet", "Sam", "Dillan"]
+let event = "Birthday"
+let messages = writeCards(names, event)
+
+
+
+
+function countDown(number){
+    for (let i = number; i >= 0; i--){
+      console.log(i)
+    }
+  }
+  
+  countDown(10)
